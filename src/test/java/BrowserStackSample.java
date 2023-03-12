610e981fc549afac414009e54072540c32263973
Sachin Bhardwaj <sachinb1750@gmail.com>
2023-03-12 23:40:11 +0530

    Removed Creds

--- src/test/java/BrowserStackSample.java
+++ src/test/java/BrowserStackSample.java
@@ -17,13 +17,11 @@ public class BrowserStackSample {
 	public void demoTest() throws MalformedURLException, InterruptedException {
 		
     	DesiredCapabilities caps = new DesiredCapabilities();
-    	
-    	// Set your access credentials
-    	caps.setCapability("browserstack.user", "ersachin_hS87en");
-    	caps.setCapability("browserstack.key", "tmyn26QYqFZzfy7qRCLh");
 
-//		caps.setCapability("browserstack.user", System.getProperty("BROWSERSTACK_USERNAME"));
-//		caps.setCapability("browserstack.key", System.getProperty("BROWSERSTACK_ACCESS_KEY"));
+    	// Set your access credentials
+
+		caps.setCapability("browserstack.user", System.getProperty("BROWSERSTACK_USERNAME"));
+		caps.setCapability("browserstack.key", System.getProperty("BROWSERSTACK_ACCESS_KEY"));
     	
     	// Set URL of the application under test
     	caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
